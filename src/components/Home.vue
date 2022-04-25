3b15cd4abb9022f6344491cbbafa2957bd4310fc
Andy <andy.evans@razor.co.uk>
2022-04-25 20:43:59 +0100

    Stuff

---
+++ src/components/Home.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="site-container">
+        <p class="mega">Andy and Liz</p>
+        <p class="sub-heading">are getting married</p>
+        <div class="splash-container">
+        </div>  
+
+        <Panel index="0" image="pic1.jpg" title="When?" content="We're not entirely sure."/>
+        <Panel index="1" image="pic2.jpg" title="Where?" content="We haven't decided yet."/>
+        <Panel index="2" image="pic3.jpg" title="What's the plan afterwards?" content="We haven't decided yet."/>
+    </div>
+</template>
+
+<script lang="ts">
+import Panel from './Panel.vue'
+export default {
+  name: 'Home',
+  components: {
+   Panel   
+  }
+};
+</script>
+
+<style scoped>
+.left-container {
+    display: flex;
+    justify-content: left;
+    flex-direction: column;
+    /* width: 80%; */
+}
+.outer-container {
+    width: auto;
+    margin: auto;
+}
+.site-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    color: #fff;
+    width: 80%;
+    margin: auto;
+    margin-bottom: 50px;
+}
+.splash-container {
+    width: 100%;
+    height: 500px;
+    background-image: url('../../public/splash4.jpg');
+    background-position: center center;
+    background-size: 100% auto;
+    background-repeat: no-repeat;
+    opacity: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 20px;
+}
+.mega {
+    font-size: 5rem;
+    margin: 0;
+    margin-bottom: 20px;
+}
+.sub-heading {
+    font-size: 1.2rem;
+    margin: 0;
+    margin-bottom: 20px;
+}
+
+@media only screen and (max-width: 1024px) {
+    .splash-container {
+        height: 200px;
+    }
+}
+</style>
---
+++ src/components/Panel.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="card-container">
+    <div v-if="index % 2 == 0">
+      <b-card no-body class="overflow-hidden" style="max-height: 300px">
+        <b-row no-gutters>
+          <b-col md="6">
+            <b-card-body :title="title">
+              <b-card-text>
+                {{content}}
+              </b-card-text>
+            </b-card-body>
+          </b-col>
+
+          <b-col md="6">
+            <b-card-img :src="image" alt="Image" class="rounded-0"></b-card-img>
+          </b-col>
+        </b-row>
+      </b-card>
+    </div>
+
+    <div v-else>
+      <b-card no-body class="overflow-hidden" style="max-height: 300px">
+        <b-row no-gutters>
+          <b-col md="6">
+            <b-card-img :src="image" alt="Image" class="rounded-0"></b-card-img>
+          </b-col>
+          <b-col md="6">
+            <b-card-body :title="title">
+              <b-card-text>
+                {{content}}
+              </b-card-text>
+            </b-card-body>
+          </b-col>
+        </b-row>
+      </b-card>
+    </div>
+  </div>
+
+</template>
+
+<script lang="ts">
+
+export default {
+  name: 'Panel',
+  props: {
+    title: String,
+    content: String,
+    image: String,
+    index: Number
+  },
+  components: {
+  }
+};
+</script>
+
+<style scoped>
+.card-container {
+  margin-top: 20px;
+}
+</style>
