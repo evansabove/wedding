f4a46419df85ccb4544c0eb457c4ce0dfa3f2da0
Andy <andy.evans@razor.co.uk>
2023-08-19 15:42:40 +0100

    New picture, plus build fix

--- website/src/views/Gifts.vue
+++ website/src/views/Gifts.vue
@@ -3,12 +3,18 @@
     <h1>Gifts</h1>
     <Panel :index="1">
       <p>In a nutshell... we already have everything we need!</p>
-      <p>
+      <p class="mb-2">
         For our honeymoon, we are planning to go to Europe on our new motorbike
         (Margaret the BMW GS), to try and get to the Italian lakes. If you wanted
         to gift us something, we would welcome some money towards pasta, pizza
         and petrol.
       </p>
+    </Panel>
+
+    <Panel :index="2">
+      <div style="text-align: center">
+        <img class="together" src="margaret.jpg" />
+      </div>
     </Panel>
   </div>
 </template>
